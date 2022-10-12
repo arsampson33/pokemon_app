7d2b19cec0c4dc768c2171a3e2f15466c3f734e2
arsampson33 <arsampson33@gmail.com>
2022-10-12 03:10:46 -0400

    added more style

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,9 +5,10 @@ class Index extends React.Component{
         const pokemons = this.props.pokemon
         return(
         <div style ={styles.container}>
-           <h1>See All the Pokemon</h1>
+           <h1 style ={styles.header}>See All Your Pokemon</h1>
+           <a  style={styles.addBtn }href='/pokemon/new'>  Catch more Pokemon!</a>
 
-           <ul>
+           <ul style={styles.links}>
             {pokemons.map((pokemon,idx) => {
                 return(
                     <li> <a href={`/pokemon/${pokemon._id}`}> {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</a> 
@@ -24,7 +25,26 @@ class Index extends React.Component{
 const styles = {
     container: {
         color: 'white',
-        backgroundColor: 'black'
+        backgroundColor: 'black',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+
+    header:{
+        color: 'red'
+    },
+
+    links:{
+        textDecoration: 'none',
+        color: 'white',
+        fontSize: '22px'
+    },
+    
+    addBtn:{
+        padding:'20px',
+        backgroundColor:'white',
+        borderRadius: '2rem'
     }
 }
 
